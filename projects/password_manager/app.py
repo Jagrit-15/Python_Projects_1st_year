a4abc1e4b69f322b8167be70678b960265f038dd
Jagrit-15 <jagritpramanick@gmail.com>
2026-04-25 16:13:28 +0530

    Adding the search button and exception handling in password manager

--- projects/password_manager/app.py
+++ projects/password_manager/app.py
@@ -2,12 +2,21 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 def save_details():
-    website = web_entry.get()
+    website = web_entry.get().title()
     email = user_entry.get()
     password = password_entry.get()
     
+    new_data = {
+                    website :
+                    {
+                        "Email" : email,
+                        "Password" : password
+                    }
+    }
+
     if len(website)==0 or len(password)==0:
         messagebox.showinfo(title="OOPS" , message="Please enter all the fields")
     else:
@@ -15,10 +24,43 @@ def save_details():
                                     f"Email/Username : {email}\nPassword : {password}")
 
         if is_ok:
-            with open("projects\password_manager\password.txt" , "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                web_entry.delete(0,END)
-                password_entry.delete(0,END)
+            try :
+                with open(r"projects\password_manager\password.json" , "r") as data_file:
+                    # Reading old data 
+                    data = json.load(data_file)
+                    # Updating old data with new data 
+                    data.update(new_data)
+
+            except(FileNotFoundError):
+                with open(r"projects\password_manager\password.json" , "w") as data_file:
+                    # saving updated data 
+                    json.dump(new_data, data_file , indent=4)
+                    web_entry.delete(0,END)
+                    password_entry.delete(0,END)
+
+            else:
+                with open(r"projects\password_manager\password.json" , "w") as data_file:
+                    # saving updated data 
+                    json.dump(data, data_file , indent=4)
+                    web_entry.delete(0,END)
+                    password_entry.delete(0,END)
+def search_details():
+    try:
+        with open(r"projects\password_manager\password.json" , "r") as data_file:
+                    details = json.load(data_file)
+        website = details[web_entry.get().title()]
+        email = website["Email"]
+        password = website["Password"]
+        messagebox.showinfo(title=web_entry.get() , message=f"Email : {email}\nPassword : {password}")
+        web_entry.delete(0,END)
+        password_entry.delete(0,END)
+    
+    except(FileNotFoundError):
+         messagebox.showerror(message="Website not added")
+
+    except(KeyError):
+         messagebox.showerror(message="Website not added")
+    
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
@@ -49,8 +91,8 @@ canvas.grid(row=0 , column=1)
 
 web_label = Label(text="Website : ")
 web_label.grid(row=1 , column=0 , sticky="e" , ipadx=5 , ipady=5)
-web_entry = Entry(width=44)
-web_entry.grid(row=1 , column=1 , columnspan=2 , sticky="e")
+web_entry = Entry(width=25)
+web_entry.grid(row=1 , column=1 , sticky="e")
 web_entry.focus()
 
 user_label = Label(text="Email/Username : ")
@@ -70,5 +112,7 @@ generate_pass.grid(row=3 , column=2 , ipadx=2)
 add = Button(text="Add" , width=37 , command=save_details)
 add.grid(row=4 , column=1 , columnspan=2, sticky="se")
 
+search = Button(text = "Search" , command=search_details , width=13)
+search.grid(row = 1 , column=2 , ipadx=2)
 window.mainloop()
 
